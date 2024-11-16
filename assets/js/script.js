08ff7e32de3f8190046922754a9bf202034c19c8
Krasi5 Todorova <krasi_todorova@yahoo.com>
2024-11-16 21:30:16 +0000

    style changes, function works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
-function checkUnits()
-
-function checkHeightEntry()
-
-function checkWeightEntry()
-
-function calculateBMI()
+function bmiCalculation() {
+    let userWeight = parseFloat(document.getElementById("weight").value);
+    let userHeight = parseFloat(document.getElementById("height").value);
+    
+    
+    if (isNaN(userHeight) || isNaN(userWeight) || userHeight <= 0 || userWeight <= 0) {
+        document.getElementById("result").innerHTML = "Please enter valid weight and height values.";
+            return;
+    }
+    let bmi = userWeight / (userHeight * userHeight);
+    let bmiType = "";
+    if (bmi < 18.5){
+        bmiType = "Underweight"
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        bmiType = "Normal Weight"
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        bmiType = "Overweight"
+    } else if (bmi >= 30){
+        bmiType = "Obesity"
+    }
+    
+    document.getElementById("result").innerHTML = 
+            "Your BMI is " + bmi.toFixed(1) + "<br>" +"You are " + bmiType
+        }
+     
