97733c1f4cebc7d067f875b1bbb96f628014c103
Krasi5 <151970609+Krasi5@users.noreply.github.com>
2025-05-30 09:51:46 +0100

    Update script.js comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 function bmiCalculation() {
+    //get values 
     let userWeight = parseFloat(document.getElementById("weight").value);
     let userHeight = parseFloat(document.getElementById("height").value);
-    
-    
+    //check correctness of the values and wrong entry message display 
     if (isNaN(userHeight) || isNaN(userWeight) || userHeight <= 0 || userWeight <= 0) {
         document.getElementById("result").innerHTML = "Please enter valid weight and height values.";
             return;
     }
+    //calculation of the BMI
     let bmi = userWeight / (userHeight * userHeight);
     let bmiType = "";
+    //set BMI categories 
     let underweightBox = document.getElementById("under");
     let normalWeightBox = document.getElementById("normal");
     let overweightBox = document.getElementById("over");
     let obesityBox = document.getElementById("obese");
+    //categories range and colour coding
     if (bmi < 18.5){
         bmiType = "Underweight"
         underweightBox.style.backgroundColor = "lightblue"
@@ -26,7 +29,7 @@ function bmiCalculation() {
         bmiType = "Obesity";
         obesityBox.style.backgroundColor = "red"
     }
-    
+    //get result and display result message
     document.getElementById("result").innerHTML = 
             "Your BMI is " + bmi.toFixed(1) + "<br>" +"Your BMI category is " + bmiType
         }
